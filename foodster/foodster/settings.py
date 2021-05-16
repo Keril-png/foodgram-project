1a5b9e30298497980e05f51ab8a2d53b2a2c6606
Keril-png <kirabramofff@gmail.com>
2021-05-16 11:32:21 +0000

    12

--- foodster/foodster/settings.py
+++ foodster/foodster/settings.py
@@ -155,3 +155,8 @@ STATICFILES_DIRS = [
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+
+#  подключаем движок filebased.EmailBackend
+EMAIL_BACKEND = "django.core.mail.backends.filebased.EmailBackend"
+# указываем директорию, в которую будут складываться файлы писем
+EMAIL_FILE_PATH = os.path.join(BASE_DIR, "sent_emails")
