11f3facf0bddc20d48afec3c644bb584faf19acd
Keril-png <kirabramofff@gmail.com>
2021-05-18 16:46:03 +0300

    image

--- foodster/static/formRecipe.js
+++ foodster/static/formRecipe.js
@@ -107,3 +107,14 @@ formDropdownItems.addEventListener('click', ingredients.dropdown);
 addIng.addEventListener('click', ingredients.addIngredient);
 
 
+var span = document.getElementsByClassName('upload-path');
+// Button
+var uploader = document.getElementsByName('upload');
+// On change
+for( item in uploader ) {
+  // Detect changes
+  uploader[item].onchange = function() {
+    // Echo filename in span
+    span[0].innerHTML = this.files[0].name;
+  }
+}
